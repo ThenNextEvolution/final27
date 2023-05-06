23ea01595326365e863d1537dae5fe983481f82e
eyita <me@ME>
2023-05-06 13:14:39 -0400

    Add project files.

---
+++ Models/nemodel.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace final27.Models
+{
+    public class Nemodel
+    {
+        [JsonProperty("copyright")]
+        public string? Url { get; set; }//copyright
+        [JsonProperty("explanation")]
+        public string? Explanation { get; set; }//explanation
+    }
+}
